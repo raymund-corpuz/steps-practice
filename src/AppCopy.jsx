6a448b9bf02426f32b32648072dbd3fbd81d069c
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-05-05 07:20:41 +0800

    Re-usable Button

---
+++ src/AppCopy.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((prev) => prev + 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((prev) => prev + 1);
+  }
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            {/* <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button> */}
+            {/* <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button> */}
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handlePrevious}
+              text="Previous"
+            />
+            <Button
+              bgColor="#7950f2"
+              textColor="#fff"
+              onClick={handleNext}
+              text="Next"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {" "}
+      {text}
+    </button>
+  );
+}
